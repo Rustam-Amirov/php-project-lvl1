e525073cb384f408c32d595a5df6856bb5621ac9
Rustam-Amirov <r.amirov@yahoo.com>
2019-10-31 00:28:24 +0300

    refactoring

--- src/games/calc.php
+++ src/games/calc.php
@@ -8,31 +8,32 @@ function calc()
 {
     $rule = 'What is the result of the expression?';
     $game = function () {
-        $num1 = rand(1, 100);
-        $num2 = rand(1, 10);
+        $firstNum = rand(1, 100);
+        $secondNum = rand(1, 10);
         $operators = ['+', '-', '*'];
         $randOpearator = $operators[rand(0, count($operators) - 1)];
-        $question = "Question: {$num1} {$randOpearator} {$num2}";
-        $correctAnswer = calculationCorrectAnswer($randOpearator, $num1, $num2);
+        $question = "Question: {$firstNum} {$randOpearator} {$secondNum}";
+        $correctAnswer = calculationCorrectAnswer($randOpearator, $firstNum, $secondNum);
         return [
             'question' => $question,
             'correctAnswer' => $correctAnswer
         ];
     };
-
-    function calculationCorrectAnswer($randOpearator, $num1, $num2)
-    {
-        switch ($randOpearator) {
-            case '+':
-                return $num1 + $num2;
-                break;
-            case '-':
-                return $num1 - $num2;
-                break;
-            case '*':
-                return $num1 * $num2;
-                break;
-        }
-    }
     run($game, $rule);
+}
+
+function calculationCorrectAnswer($randOpearator, $firstNum, $secondNum)
+{
+        
+    switch ($randOpearator) {
+        case '+':
+            return $firstNum + $secondNum;
+            break;
+        case '-':
+            return $firstNum - $secondNum;
+            break;
+        case '*':
+            return $firstNum * $secondNum;
+            break;
+    }
 }
